33afcceee11cb1256dfb4434477b82dcb829989b
Aleklz90 <novogogosha@gmail.com>
2024-09-26 18:43:45 +0300

    ts eslint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import styles from "./page.module.css"
-import Navigation from "../components/Navigation/Navigation";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "VPN service",
