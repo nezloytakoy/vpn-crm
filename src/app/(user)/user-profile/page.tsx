6c39d9c4978de5b74363248f515524ef5eb4bf4b
Aleklz90 <novogogosha@gmail.com>
2024-09-26 19:38:24 +0300

    user profile adaptation

--- src/app/(user)/user-profile/page.tsx
+++ src/app/(user)/user-profile/page.tsx
@@ -9,17 +9,17 @@ import Image from 'next/image';
 const WaveComponent = () => {
     return (
         <div>
-            <div style={{ position: 'relative', height: '300px', overflow: 'hidden' }}>
+            <div style={{ position: 'relative', height: '250px', overflow: 'hidden' }}>
                 <Wave
                     fill="white"
                     paused={false}
                     options={{
-                        height: 20,
-                        amplitude: 40,
+                        height: 10,
+                        amplitude: 20,
                         speed: 0.15,
                         points: 3,
                     }}
-                    style={{ position: 'absolute', bottom: '-10px', width: '100%' }}
+                    style={{ position: 'absolute', bottom: '-70px', width: '100%' }}
                 />
                 <div className={styles.topbotom}>
                     <div className={styles.greetings}>
@@ -45,8 +45,8 @@ const WaveComponent = () => {
                             <Image
                                 src="https://92eaarerohohicw5.public.blob.vercel-storage.com/ai-one-JV9mpH87gcyosXasiIjyWSapEkqbaQ.png"
                                 alt="avatar"
-                                width={100}
-                                height={100}
+                                width={90}
+                                height={90}
                                 className={styles.ai}
                             />
                             <p className={styles.text}>Only AI</p>
@@ -55,8 +55,8 @@ const WaveComponent = () => {
                             <Image
                                 src="https://92eaarerohohicw5.public.blob.vercel-storage.com/ai-three-cGoXQPamKncukOKvfhxY8Gwhd4xKpO.png"
                                 alt="avatar"
-                                width={120}
-                                height={140}
+                                width={100}
+                                height={100}
                                 className={styles.ai}
                             />
                             <p className={styles.text}>AI <br/>5 hours/month</p>
@@ -65,8 +65,8 @@ const WaveComponent = () => {
                             <Image
                                 src="https://92eaarerohohicw5.public.blob.vercel-storage.com/GIU%20AMA%20255-02-kdT58Hckjc871B2UsslUF7ZrAg9SAi.png"
                                 alt="avatar"
-                                width={100}
-                                height={115}
+                                width={90}
+                                height={105}
                                 className={styles.ai}
                             />
                             <p className={styles.text}>AI 14 hours/month</p>
@@ -79,7 +79,7 @@ const WaveComponent = () => {
                             width={80}
                             height={80}
                             className={styles.ai}
-                        />AI 30 hours/month</div>
+                        /> <p className={styles.aitext}>AI 30 hours/month</p></div>
                         <div className={styles.block}><Image
                             src="https://92eaarerohohicw5.public.blob.vercel-storage.com/f3BR23dMA4SapXd0Jg-TxjGLHkcqjJKq8zONZRfnlVilJLKGw.gif"
                             alt="avatar"
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,55 +5,54 @@ import { IonIcon } from '@ionic/react';
 import { personOutline, chatbubbleOutline } from 'ionicons/icons';
 
 const Navigation = () => {
-  const Menus = [
-    { name: "Profile", icon: personOutline, dis: "left-1/4" },
-    { name: "Message", icon: chatbubbleOutline, dis: "left-3/4" },
-  ];
-  const [active, setActive] = useState(0);
+    const Menus = [
+        { name: "Profile", icon: personOutline, dis: "left-1/4" },
+        { name: "Message", icon: chatbubbleOutline, dis: "left-3/4" },
+    ];
+    const [active, setActive] = useState(0);
 
-  return (
-    <div className="bg-[#4581E9] max-h-[4.4rem] px-12 rounded-t-xl w-full font-custom"> {/* Применяем кастомный шрифт */}
-      <ul className="flex justify-between relative w-full">
-        <span
-          className={`bg-rose-600 duration-500 ${active === 0 ? 'left-1/4' : 'left-3/4'} border-4 border-white h-16 w-16 absolute -top-5 rounded-full transform -translate-x-1/2`}
-        >
-          <span
-            className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] 
+    return (
+        <div className="bg-[#4581E9] fixed bottom-0 left-0 right-0 max-h-[6rem] px-12 w-full font-custom z-50 border-t-8 border-white">
+            <ul className="flex justify-between relative w-full">
+
+                <span
+                    className={`bg-rose-600 duration-500 ${active === 0 ? 'left-1/4' : 'left-3/4'} border-4 border-white h-16 w-16 absolute -top-5 rounded-full transform -translate-x-1/2`}
+                >
+                    <span
+                        className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] 
           rounded-tr-[11px] shadow-myShadow1"
-          ></span>
-          <span
-            className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] 
+                    ></span>
+                    <span
+                        className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] 
           rounded-tl-[11px] shadow-myShadow2"
-          ></span>
-        </span>
-        {Menus.map((menu, i) => (
-          <li key={i} className="w-1/2 text-center">
-            <a
-              className="flex flex-col items-center pt-6"
-              onClick={() => setActive(i)}
-            >
-              <span
-                className={`text-xl font-bold text-white cursor-pointer duration-500 ${
-                  i === active && "-mt-6 text-white"
-                }`}
-              >
-                <IonIcon icon={menu.icon} />
-              </span>
-              <span
-                className={`text-white font-bold ${
-                  active === i
-                    ? "translate-y-4 duration-700 opacity-100"
-                    : "opacity-0 translate-y-10"
-                } `}
-              >
-                {menu.name}
-              </span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+                    ></span>
+                </span>
+                {Menus.map((menu, i) => (
+                    <li key={i} className="w-1/2 text-center">
+                        <a
+                            className="flex flex-col items-center pt-6"
+                            onClick={() => setActive(i)}
+                        >
+                            <span
+                                className={`text-xl font-bold text-white cursor-pointer duration-500 ${i === active && "-mt-6 text-white"
+                                    }`}
+                            >
+                                <IonIcon icon={menu.icon} />
+                            </span>
+                            <span
+                                className={`text-white font-bold ${active === i
+                                        ? "translate-y-4 duration-700 opacity-100"
+                                        : "opacity-0 translate-y-10"
+                                    } `}
+                            >
+                                {menu.name}
+                            </span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Navigation;
