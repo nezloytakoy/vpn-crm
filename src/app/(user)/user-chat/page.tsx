00e79961396414c907ea897709c8116a6c3bdf0c
Aleklz90 <novogogosha@gmail.com>
2024-09-27 14:38:50 +0300

    unused imports removed

--- src/app/(user)/user-chat/page.tsx
+++ src/app/(user)/user-chat/page.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import styles from "./chat.module.css"
+import Image from 'next/image'
+import Wave from 'react-wavify';
 
 function page() {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div>
+            <div style={{ position: 'relative', height: '250px', overflow: 'hidden' }}>
+                <Wave
+                    fill="white"
+                    paused={false}
+                    options={{
+                        height: 10,
+                        amplitude: 20,
+                        speed: 0.15,
+                        points: 3,
+                    }}
+                    style={{ position: 'absolute', bottom: '-70px', width: '100%' }}
+                />
+                <div className={styles.topbotom}>
+                    <div className={styles.greetings}>
+                        Рефералы
+                        <div className={styles.avatarbox}>
+                            <Image
+                                src="https://92eaarerohohicw5.public.blob.vercel-storage.com/person-ECvEcQk1tVBid2aZBwvSwv4ogL7LmB.svg"
+                                alt="avatar"
+                                width={110}
+                                height={110}
+                                className={styles.avatar}
+                            />
+                            <p className={styles.name}> John Doe </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default page
