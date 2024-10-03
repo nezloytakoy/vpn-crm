9680d7cd016ab37a2ea5ddd1b88e2dd632947868
Aleklz90 <novogogosha@gmail.com>
2024-10-03 15:32:00 +0300

    assistents page adaptation

--- src/app/(admin)/admin/supports/page.tsx
+++ src/app/(admin)/admin/supports/page.tsx
@@ -1,34 +1,105 @@
-
 "use client"
 
 import React, { useState } from 'react';
 import styles from './Users.module.css';
 
 function Page() {
-    const [isToggled, setIsToggled] = useState(false); // Состояние для переключателя
+    const [isToggled, setIsToggled] = useState(false);
+    const [mode, setMode] = useState('single');
 
     const handleToggleChange = () => {
-        setIsToggled(!isToggled); // Меняем состояние при клике на переключатель
+        setIsToggled(!isToggled);
     };
+
+    const handleModeChange = () => {
+        setMode(mode === 'single' ? 'interval' : 'single');
+    };
+
+
+    const [modetwo, setModetwo] = useState('single');
+
+    const handleModeChangetwo = () => {
+        setModetwo(modetwo === 'single' ? 'interval' : 'single');
+    };
+
+    const [inputValue, setInputValue] = useState('100'); // Начальное значение "100"
+
+
+
+
 
     return (
         <div className={styles.main}>
             <div className={styles.columnblock}>
-                <div className={styles.messageblock}>
+
+                <div className={styles.messageboxthree}>
                     <h1 className={styles.title}>Уведомления всем ассистентам</h1>
-                    <div className={styles.messagebox}>
-                        <h1 className={styles.undertitle}>Форма для сообщения</h1>
-                        <textarea className={styles.input} placeholder="Сообщение" />
-                        {/* Добавляем кнопку "Отправить" */}
-                        <button className={styles.submitButton}>Отправить</button>
-                    </div>
+                    <h1 className={styles.undertitle}>Форма для сообщения</h1>
+                    <textarea className={styles.input} placeholder="Сообщение" />
+                    <button className={styles.submitButton}>Отправить</button>
                 </div>
+
+
+
+                <div className={styles.messagebox}>
+                    <h1 className={styles.gifttitle}>Максимальное количество отказов</h1>
+                    <h1 className={styles.undertitletwo}>Введите количество</h1>
+                    <div className={`${styles.inputContainertwo} ${isToggled ? styles.active : ''}`}>
+                        <input type="text" className={styles.inputFieldtwo} placeholder="7" />
+                        <span className={`${styles.label} ${isToggled ? styles.activeLabel : ''}`}>Отказов</span>
+                    </div>
+                    <button className={styles.submitButtontwo}>Подтвердить</button>
+                </div>
+                <div className={styles.messageboxfour}>
+                    <h1 className={styles.gifttitle}>В качестве награды ассистент получает</h1>
+                    <h1 className={styles.undertitletwo}>Введите количество</h1>
+                    <div className={`${styles.inputContainertwo} ${isToggled ? styles.active : ''}`}>
+                        <input type="text" className={styles.inputFieldtwo} placeholder="10" />
+                        <span className={`${styles.label} ${isToggled ? styles.activeLabel : ''}`}>Койнов</span>
+                    </div>
+                    <div className={styles.userblock}>
+                        <h1 className={styles.gifttitletwo}>В качестве награды пользователь получает</h1>
+                    </div>
+                    <h1 className={styles.undertitletwo}>Введите количество</h1>
+                    <div className={`${styles.inputContainertwo} ${isToggled ? styles.active : ''}`}>
+                        <input type="text" className={styles.inputFieldtwo} placeholder="10" />
+                        <span className={`${styles.label} ${isToggled ? styles.activeLabel : ''}`}>Койнов</span>
+                    </div>
+                    <div className={styles.userblock}>
+                        <h1 className={styles.gifttitletwo}>Количество запросов для награды</h1>
+                    </div>
+                    <h1 className={styles.undertitletwo}>Введите количество</h1>
+                    <div className={`${styles.inputContainertwo} ${isToggled ? styles.active : ''}`}>
+                        <input
+                            type="text"
+                            className={styles.inputFieldtwo}
+                            placeholder="100"
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
+                        <span className={`${styles.label} ${isToggled ? styles.activeLabel : ''}`}>Запросов</span>
+                    </div>
+                    <div className={styles.modeSlider}>
+                        <span className={modetwo === 'single' ? styles.activeMode : ''}>Разово</span>
+                        <label className={styles.switchtwo}>
+                            <input type="checkbox" checked={modetwo === 'interval'} onChange={handleModeChangetwo} />
+                            <span className={styles.slider}></span>
+                        </label>
+                        <span className={modetwo === 'interval' ? styles.activeMode : ''}>
+                            Через каждые {inputValue}
+                        </span>
+                    </div>
+                    <button className={styles.submitButtontwo}>Подтвердить</button>
+
+                </div>
+
             </div>
 
             <div className={styles.columnblock}>
-                <div className={styles.messageblock}>
-                    <h1 className={styles.notitle}></h1>
-                    <div className={styles.messageboxtwo}>
+
+                <h1 className={styles.notitle}></h1>
+                <div className={styles.messageboxtwo}>
+                    <div className={styles.contentbox}>
                         <h1 className={styles.gifttitle}>Подарочные койны</h1>
                         <div className={styles.togglebox}>
                             <label className={styles.switch}>
@@ -37,13 +108,59 @@ function Page() {
                             </label>
                             <span className={styles.label}>Включить эту опцию</span>
                         </div>
-                        <h1 className={styles.undertitle}>Введите количество</h1>
+                    </div>
+                    <div className={styles.contentbox}>
+                        <h1 className={styles.undertitletwo}>Введите количество</h1>
                         <div className={`${styles.inputContainer} ${isToggled ? styles.active : ''}`}>
                             <input type="text" className={styles.inputField} placeholder="10000" />
                             <span className={`${styles.label} ${isToggled ? styles.activeLabel : ''}`}>Койнов</span>
                         </div>
+
+
+                        <div className={styles.modeSlider}>
+                            <span className={mode === 'single' ? styles.activeMode : ''}>Разово</span>
+                            <label className={styles.switchtwo}>
+                                <input type="checkbox" checked={mode === 'interval'} onChange={handleModeChange} />
+                                <span className={styles.slider}></span>
+                            </label>
+                            <span className={mode === 'interval' ? styles.activeMode : ''}>Через</span>
+                        </div>
+
+
+                        {mode === 'interval' && (
+                            <div className={`${styles.inputContainer} ${styles.intervalInputContainer}`}>
+                                <input type="text" className={styles.inputField} placeholder="5" />
+                                <span className={styles.label}>Запросов</span>
+                            </div>
+                        )}
+                        <button className={styles.submitButton}>Подтвердить</button>
                     </div>
                 </div>
+                <div className={styles.messagebox}>
+                    <h1 className={styles.gifttitle}>Время на принятие запроса</h1>
+                    <h1 className={styles.undertitletwo}>Введите количество</h1>
+                    <div className={`${styles.inputContainertwo} ${isToggled ? styles.active : ''}`}>
+                        <input type="text" className={styles.inputFieldtwo} placeholder="60" />
+                        <span className={`${styles.label} ${isToggled ? styles.activeLabel : ''}`}>Секунд</span>
+                    </div>
+                    <button className={styles.submitButtontwo}>Подтвердить</button>
+                </div>
+                <div className={styles.messageboxfive}>
+                    <h1 className={styles.title}>Текст кнопок</h1>
+
+
+                    <div className={styles.fakeButton} contentEditable="true">
+                        Лимиты
+                    </div>
+
+
+                    <div className={styles.fakeButton} contentEditable="true">
+                        Инструкции
+                    </div>
+
+                    <button className={styles.submitButton}>Подтвердить</button>
+                </div>
+
             </div>
         </div>
     );
