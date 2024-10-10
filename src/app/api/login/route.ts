cb5b5724964bbd75b1cd8794a15f34a5a0ce8f7f
Aleklz90 <novogogosha@gmail.com>
2024-10-10 12:49:51 +0300

    moderator admin panel login logic

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,41 +1,37 @@
-import validatePassword from "@/helpers/validatePassword";
-import validateEmail from "@/helpers/validateEmail";
 import prisma from "../../../../lib/server/prisma";
 import bcrypt from 'bcryptjs';
 import * as jose from 'jose';
 
 export async function POST(request: Request) {
- 
   const body = await request.json();
   const { email, password } = body;
 
- 
-  if (!validateEmail(email)) {
-    return new Response(
-      JSON.stringify({
-        error: "Invalid email format",
-      }),
-      { status: 400 }
-    );
-  }
-
-  if (!validatePassword(password)) {
-    return new Response(
-      JSON.stringify({
-        error: "Invalid password format",
-      }),
-      { status: 400 }
-    );
-  }
-
-  
-  const user = await prisma.admin.findFirst({
+  // Поиск пользователя среди администраторов
+  const admin = await prisma.admin.findFirst({
     where: {
       email,
     },
   });
 
-  if (!user) {
+  // Поиск пользователя среди модераторов
+  const moderator = await prisma.moderator.findFirst({
+    where: {
+      login: email,
+    },
+  });
+
+  // Проверка пароля для администратора или модератора
+  let user = admin || moderator;
+  let isCorrectPassword = false;
+
+  if (admin) {
+    isCorrectPassword = bcrypt.compareSync(password, admin.password);
+  } else if (moderator) {
+    isCorrectPassword = bcrypt.compareSync(password, moderator.password);
+  }
+
+  // Если ни пользователь не найден, ни пароль не совпал
+  if (!user || !isCorrectPassword) {
     return new Response(
       JSON.stringify({
         error: "Invalid email or password",
@@ -44,19 +40,7 @@ export async function POST(request: Request) {
     );
   }
 
-
-  const isCorrectPassword = bcrypt.compareSync(password, user.password);
-
-  if (!isCorrectPassword) {
-    return new Response(
-      JSON.stringify({
-        error: "Invalid email or password",
-      }),
-      { status: 400 }
-    );
-  }
-
-
+  // Проверяем наличие JWT секретного ключа
   if (!process.env.JWT_SECRET) {
     console.error("JWT_SECRET is not defined");
     return new Response(
@@ -67,16 +51,15 @@ export async function POST(request: Request) {
     );
   }
 
-  
+  // Генерация JWT токена
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
   const alg = "HS256";
 
   const jwt = await new jose.SignJWT({})
     .setProtectedHeader({ alg })
     .setExpirationTime("72h")
-    .setSubject(user.id.toString())
+    .setSubject(user.id.toString()) // Теперь проверка на наличие user гарантирует, что id будет существовать
     .sign(secret);
-
 
   return new Response(JSON.stringify({ token: jwt }), { status: 200 });
 }
