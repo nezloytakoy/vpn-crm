e2ac210fc200c6fd524b8f1782a7be123ef91f81
Aleklz90 <novogogosha@gmail.com>
2024-10-11 19:45:30 +0300

    complaints bigint to string converting

--- src/app/api/get-complaints/route.ts
+++ src/app/api/get-complaints/route.ts
@@ -11,14 +11,24 @@ export async function GET() {
         id: true,
         reason: true,  // Жалоба (причина арбитража)
         userId: true,
-        userNickname: true, // Поле userNickname
+        userNickname: true,
         assistantId: true,
-        assistantNickname: true, // Поле assistantNickname
+        assistantNickname: true,
       },
     });
 
+    // Преобразуем BigInt в строки для сериализации
+    const serializedComplaints = complaints.map(complaint => ({
+      id: complaint.id.toString(),
+      reason: complaint.reason,
+      userId: complaint.userId.toString(),
+      userNickname: complaint.userNickname,
+      assistantId: complaint.assistantId.toString(),
+      assistantNickname: complaint.assistantNickname,
+    }));
+
     // Возвращаем данные в формате JSON
-    return NextResponse.json(complaints);
+    return NextResponse.json(serializedComplaints);
   } catch (error) {
     console.error('Ошибка при получении жалоб:', error);
     return NextResponse.json({ error: 'Ошибка сервера' }, { status: 500 });
