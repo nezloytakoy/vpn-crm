012b0200a7d70e04e5dd789da068734a3b49c55a
Aleklz90 <novogogosha@gmail.com>
2024-10-17 19:12:25 +0300

    get assistant route commented

--- src/app/api/get-assistant/route.ts
+++ src/app/api/get-assistant/route.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 export async function GET(request: NextRequest) {
 
