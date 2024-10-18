934a362d913cf1103db04e146febff0d81f62876
Aleklz90 <novogogosha@gmail.com>
2024-10-18 13:18:46 +0300

    get assistant route plug

--- src/app/api/get-assistant/route.ts
+++ src/app/api/get-assistant/route.ts
@@ -1,19 +1,19 @@
 // import { PrismaClient } from "@prisma/client";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // const prisma = new PrismaClient()
 
-// export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
 
-//     const url = new URL(request.url)
+    const url = new URL(request.url)
 
-//     const assistantId = url.searchParams.get('assistantId')
+    const assistantId = url.searchParams.get('assistantId')
 
-//     if (!assistantId) {
-//         return NextResponse.json({ error: "Не передан айди ассистента" }, { status: 400 })
-//     }
+    if (!assistantId) {
+        return NextResponse.json({ error: "Не передан айди ассистента" }, { status: 400 })
+    }
 
-//     try {
+    try {
 
 //         const assistant = await prisma.assistant.findUnique({
 //             where: { telegramId: BigInt(assistantId) },
@@ -21,7 +21,7 @@
 //                 orderNumber: true,
 //                 username: true,
 //                 telegramId: true,
-
+                
 
 
                 
@@ -48,14 +48,14 @@
 //             // получить у ассистента: порядковый номер, юзернейм, айди, количество запросов за все время, месяц, неделю, сутки, количество отказов, количество жалоб, количество проигнорированных запросов, среднее время ответа в минутах, количество выходов на линию, средняя продолжительность работы
 //         })
 
-//         //данные про каждый запрос с логами
+        //данные про каждый запрос с логами
 
-//         //история транзакций по койнам
+        //история транзакций по койнам
 
-//         // подопечные ассистента
+        // подопечные ассистента
 
-//     } catch (error) {
-//         console.log(error)
-//         return NextResponse.json({ error: "Ошибка при получении ассистента" }, { status: 500 })
-//     }
-// }
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: "Ошибка при получении ассистента" }, { status: 500 })
+    }
+}
