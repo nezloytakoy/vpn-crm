ba0b475c98f9621972bd920718a321ec7b2d2655
Aleklz90 <novogogosha@gmail.com>
2024-10-30 20:45:17 +0200

    update assistant request subscription names updated

--- src/app/api/update-assistant-requests/route.ts
+++ src/app/api/update-assistant-requests/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
   try {
     const { first, second, third, fourth } = await request.json();
 
-    
+    // Проверка, что все значения являются числами
     if (
       typeof first !== 'number' ||
       typeof second !== 'number' ||
@@ -17,24 +17,24 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: 'All four numbers are required' }, { status: 400 });
     }
 
-    
+    // Обновление подписок по новым именам
     await prisma.subscription.updateMany({
-      where: { name: 'AI + 5 запросов ассистенту' },
+      where: { name: 'FIRST' },
       data: { assistantRequestCount: first },
     });
 
     await prisma.subscription.updateMany({
-      where: { name: 'AI + 14 запросов ассистенту' },
+      where: { name: 'SECOND' },
       data: { assistantRequestCount: second },
     });
 
     await prisma.subscription.updateMany({
-      where: { name: 'AI + 30 запросов ассистенту' },
+      where: { name: 'THIRD' },
       data: { assistantRequestCount: third },
     });
 
     await prisma.subscription.updateMany({
-      where: { name: 'Только AI' },
+      where: { name: 'FOURTH' },
       data: { assistantRequestCount: fourth },
     });
 
