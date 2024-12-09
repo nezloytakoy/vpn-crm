79e761b1896d67ab9d3dcb1a936106596f8303b7
Aleklz90 <novogogosha@gmail.com>
2024-12-09 20:49:58 +0200

    admin panel give subscription logic update

--- src/app/api/get-personal-requests/route.ts
+++ src/app/api/get-personal-requests/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"; 
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export const revalidate = 1;
@@ -19,11 +19,11 @@ export async function GET(request: NextRequest) {
 
         // Находим все UserTariff для данного пользователя
         const userTariffs = await prisma.userTariff.findMany({
-          where: { userId: telegramIdBigInt },
+            where: { userId: telegramIdBigInt },
         });
 
         if (!userTariffs || userTariffs.length === 0) {
-          return NextResponse.json({ error: 'Нет тарифов для пользователя.' }, { status: 404 });
+            return NextResponse.json({ error: 'Нет тарифов для пользователя.' }, { status: 404 });
         }
 
         const now = new Date();
@@ -31,22 +31,24 @@ export async function GET(request: NextRequest) {
         const activeTariffs = userTariffs.filter(tariff => new Date(tariff.expirationDate) > now);
 
         if (activeTariffs.length === 0) {
-          return NextResponse.json({ error: 'У пользователя нет активных тарифов.' }, { status: 404 });
+            return NextResponse.json({ error: 'У пользователя нет активных тарифов.' }, { status: 404 });
         }
 
         // Суммируем оставшиеся запросы ассистента и ИИ
         const totalAssistantRequests = activeTariffs.reduce(
-          (sum, tariff) => sum + tariff.remainingAssistantRequests,
-          0
+            (sum, tariff) => sum + tariff.remainingAssistantRequests,
+            0
         );
         const totalAIRequests = activeTariffs.reduce(
-          (sum, tariff) => sum + tariff.remainingAIRequests,
-          0
+            (sum, tariff) => sum + tariff.remainingAIRequests,
+            0
         );
 
         return NextResponse.json({
-          assistantRequests: totalAssistantRequests,
-          aiRequests: totalAIRequests
+            requests: {
+                assistantRequests: totalAssistantRequests,
+                aiRequests: totalAIRequests
+            }
         });
 
     } catch (error) {
